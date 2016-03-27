6a3834e7e8caeb3cd8a6056a1c39c159b66631c1
Rasesh Patel <Rasesh@shoptiques.com>
2016-03-26 21:39:21 -0400

    Move some stuff around

---
+++ chap3/ReverseLL.java
@@ -0,0 +1,16 @@
+public class ReverseLL {
+  public static void main(String[] args) {
+    LL node = new LL(null, "obh1");
+    System.out.println("Runs");
+  }
+
+  class LL {
+    LL node;
+    Object data;
+
+    public LL(LL node, Object data) {
+      this.node = node;
+      this.data = data;
+    }
+  }
+}
